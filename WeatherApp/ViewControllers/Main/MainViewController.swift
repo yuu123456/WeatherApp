4003b348a5c5989f1df3acda09fdce1bba815025
yuu123456 <yuukunn3777@gmail.com>
2023-05-24 00:13:39 +0900

    コードによる制約見直し

--- WeatherApp/ViewControllers/Main/MainViewController.swift
+++ WeatherApp/ViewControllers/Main/MainViewController.swift
@@ -17,49 +17,54 @@ class MainViewController: UIViewController {
     let buttonCornerRadius: CGFloat = 8
     let buttonBGColor = UIColor.orange
     let buttonTintColor = UIColor.white
+    let buttonLayoutX: CGFloat = 100
+    let buttonLayoutY: CGFloat = 100
 
+    //Buttonのレイアウトを設定
     func buttonLayout(button: UIButton) {
         // ボタンの制約を設定
         button.translatesAutoresizingMaskIntoConstraints = false
 
-        // ボタンのサイズを画像と文字に合わせる制約
-        let imageWidthConstraint = button.widthAnchor.constraint(equalTo: button.imageView!.widthAnchor)
-        let textWidthConstraint = button.widthAnchor.constraint(equalTo: button.titleLabel!.widthAnchor)
-
-
+        //各プロパティを宣言
         let centerXConstraint = button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        let centerYConstraint = button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        var centerYConstraint: NSLayoutConstraint = NSLayoutConstraint()
 
+        let leading = button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: buttonLayoutX)
+        let trailing = button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -buttonLayoutX)
+
+        if button == selectButton {
+            centerYConstraint = button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -buttonLayoutY)
+        } else if button == getLocationButton {
+            centerYConstraint = button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: buttonLayoutY)
+        }
         // 制約を有効にする
-        NSLayoutConstraint.activate([imageWidthConstraint, textWidthConstraint, centerXConstraint, centerYConstraint])
+        NSLayoutConstraint.activate([centerXConstraint, centerYConstraint, leading, trailing])
+    }
 
-//        let leading = button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100)
-//        let trailing = button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100)
-//        let top = button.topAnchor.constraint(equalTo:  view.topAnchor, constant: 100)
-//        let bottom = button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100)
-//        NSLayoutConstraint.activate([leading, trailing, top, bottom])
+    //Buttonの外観プロパティを設定
+    func buttonSetting(button: UIButton) {
+        button.backgroundColor = buttonBGColor
+        button.tintColor = buttonTintColor
+
+        button.layer.cornerRadius = buttonCornerRadius
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        if button == selectButton {
+            button.setImage(selectButtonImage, for: .normal)
+        } else if button == getLocationButton {
+            button.setImage(getLocationButtonImage, for: .normal)
+        }
+
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //Buttonのプロパティを設定。（同じ設定の箇所は、まとめられないか要検討。
-        selectButton.backgroundColor = buttonBGColor
-        selectButton.tintColor = buttonTintColor
-        selectButton.setImage(selectButtonImage, for: .normal)
-        selectButton.layer.cornerRadius = buttonCornerRadius
-        selectButton.titleLabel?.adjustsFontSizeToFitWidth = true
-        selectButton.translatesAutoresizingMaskIntoConstraints = false
-
-        getLocationButton.backgroundColor = buttonBGColor
-        getLocationButton.tintColor = buttonTintColor
-        getLocationButton.setImage(getLocationButtonImage, for: .normal)
-        getLocationButton.layer.cornerRadius = buttonCornerRadius
-        getLocationButton.titleLabel?.adjustsFontSizeToFitWidth = true
-        getLocationButton.translatesAutoresizingMaskIntoConstraints = false
-
+        buttonSetting(button: selectButton)
+        buttonSetting(button: getLocationButton)
         buttonLayout(button: selectButton)
-//        buttonLayout(button: getLocationButton)
+        buttonLayout(button: getLocationButton)
 
     }
 
