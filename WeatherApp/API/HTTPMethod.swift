c0a971d0553a82f7af95066df338b8b3d6fab3e3
yuu123456 <yuukunn3777@gmail.com>
2023-06-22 17:21:26 +0900

    HTTPメソッド不要分削除

--- WeatherApp/API/HTTPMethod.swift
+++ WeatherApp/API/HTTPMethod.swift
@@ -10,11 +10,4 @@ import Foundation
 public enum HTTPMethod: String {
     case get = "GET"
     case post = "POST"
-    case put = "PUT"
-    case head = "HEAD"
-    case delete = "DELETE"
-    case patch = "PATCH"
-    case trace = "TRACE"
-    case options = "OPTIONS"
-    case connect = "CONNECT"
 }
