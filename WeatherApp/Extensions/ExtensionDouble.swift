7d7b3d9e5312e770652e4e1508da47810f3894ca
yuu123456 <yuukunn3777@gmail.com>
2023-06-26 18:22:23 +0900

    roundedValueを定数に変更

--- WeatherApp/Extensions/ExtensionDouble.swift
+++ WeatherApp/Extensions/ExtensionDouble.swift
@@ -11,7 +11,7 @@ extension Double {
     /// 小数第2で四捨五入するメソッド
     func roundToSecondDecimalPlace() -> Double {
         // 通常は小数第一でしか四捨五入できないため、10倍してから四捨五入→10で割る必要がある。
-        var roundedValue: Double = (self * 10).rounded() / 10
+        let roundedValue: Double = (self * 10).rounded() / 10
         return roundedValue
     }
 }
