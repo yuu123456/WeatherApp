dc5eebc8d1492bb7869142023085941472232792
yuu123456 <yuukunn3777@gmail.com>
2023-06-28 20:07:07 +0900

    APIに関するプロパティをstatic化

--- WeatherApp/API/OpenWeatherMapAPI.swift
+++ WeatherApp/API/OpenWeatherMapAPI.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 public final class OpenWeatherMapAPI {
-    private let apiKey: String = "5dfc577c1d7d94e9e23a00431582f1ac"
+    static private let apiKey: String = "5dfc577c1d7d94e9e23a00431582f1ac"
     // 摂氏度にするオプション
-    private let units: String = "metric"
+    static private let units: String = "metric"
     // 日本語にするオプション
-    private let lang: String = "ja"
+    static private let lang: String = "ja"
     // 取得するデータ数を制限するオプション（３時間＊８個　＝　２４時間分）
-    private let cnt: String = "8"
+    static private let cnt: String = "8"
 
     /// 緯度経度を元に、天気情報を取得するAPI
     public struct SearchWeatherData: APIRequest {
@@ -26,10 +26,10 @@ public final class OpenWeatherMapAPI {
         public var queryItems: [URLQueryItem] {
             return [URLQueryItem(name: "lat", value: String(latitude)),
                     URLQueryItem(name: "lon", value: String(longitude)),
-                    URLQueryItem(name: "appid", value: OpenWeatherMapAPI().apiKey),
-                    URLQueryItem(name: "units", value: OpenWeatherMapAPI().units),
-                    URLQueryItem(name: "lang", value: OpenWeatherMapAPI().lang),
-                    URLQueryItem(name: "cnt", value: OpenWeatherMapAPI().cnt)]
+                    URLQueryItem(name: "appid", value: OpenWeatherMapAPI.apiKey),
+                    URLQueryItem(name: "units", value: OpenWeatherMapAPI.units),
+                    URLQueryItem(name: "lang", value: OpenWeatherMapAPI.lang),
+                    URLQueryItem(name: "cnt", value: OpenWeatherMapAPI.cnt)]
         }
     }
     /// 都市名を元に、天気情報を取得するAPI
@@ -40,10 +40,10 @@ public final class OpenWeatherMapAPI {
 
         public var queryItems: [URLQueryItem] {
             return [URLQueryItem(name: "q", value: cityName),
-                    URLQueryItem(name: "appid", value: OpenWeatherMapAPI().apiKey),
-                    URLQueryItem(name: "units", value: OpenWeatherMapAPI().units),
-                    URLQueryItem(name: "lang", value: OpenWeatherMapAPI().lang),
-                    URLQueryItem(name: "cnt", value: OpenWeatherMapAPI().cnt)]
+                    URLQueryItem(name: "appid", value: OpenWeatherMapAPI.apiKey),
+                    URLQueryItem(name: "units", value: OpenWeatherMapAPI.units),
+                    URLQueryItem(name: "lang", value: OpenWeatherMapAPI.lang),
+                    URLQueryItem(name: "cnt", value: OpenWeatherMapAPI.cnt)]
         }
     }
 }
